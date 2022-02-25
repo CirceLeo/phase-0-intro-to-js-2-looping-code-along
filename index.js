c89cb1b2ba4eab1a7e917c4b308508eef38ed0d6
Calypso Leonard <calypsorleonard@gmail.com>
2022-02-25 10:20:32 -0500

    commit tree 39ad55b26b5333ba1c6fad9cf7e4482a72f4ad4d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+    let cardArray = []
+    for (let i = 0; i < namesArray.length; i++){
+        let tempMessage = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`
+        cardArray.push(tempMessage)
+    }
+    return cardArray
+}
+
+function countDown(int){
+    let counter = int;
+    while (counter >=0){
+        console.log(counter--)
+    }
+}
